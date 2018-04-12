b0765e67b894132920e940e80379ab944850462d
brickrop2 <34317184+brickrop2@users.noreply.github.com>
2018-04-12 11:05:36 -0800

    for loop for finding flush again

--- PokerHands.py
+++ PokerHands.py
@@ -71,7 +71,8 @@ def check_hand(card1,card2,card3,card4,card5,card6,card7):
         find = False
         for y in range(0, 6):
             if allcards[x][0] == allcards[y][0] and x != y:
-                #print(x, y, number)
+                #need to omit instances where a previous match has been made
+                print(allcards[x][0], x, y)
                 number += 1
             if number == 5:
                 print("You are suited!")
@@ -92,8 +93,8 @@ preflop1 = preflop('c', 1)
 preflop2 = preflop('d', 1)
 preflop_hand(preflop1.card[0], preflop1.card[1], preflop2.card[0], preflop2.card[1])
 
-flop1 = community_card('c', 1)
-flop2 = community_card('c', 13)
+flop1 = community_card('d', 1)
+flop2 = community_card('d', 13)
 flop3 = community_card('h', 12)
 turn = community_card('h', 11)
 river = community_card('h', 10)
