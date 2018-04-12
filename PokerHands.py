539431500f7b378a74b4c3ea4a59130f6fa25cc5
brickrop2 <34317184+brickrop2@users.noreply.github.com>
2018-04-12 08:26:45 -0800

    first copy

---
+++ PokerHands.py
@@ -0,0 +1,108 @@
+'''
+Notation
+c : clubs
+d : diamonds
+h : hearts
+s : spades
+1 : ace
+2 : two
+3 : three
+4 : four
+5 : five
+6 : six
+7 : seven
+8 : eight
+9 : nine
+10 : ten
+11 : jack
+12 : queen
+13 : king
+
+Code:
+place suit first, then number for card arrays
+
+'''
+
+
+
+class community_card:
+
+    def __init__(self, suit, num):
+        self.suit = suit
+        self.num = num
+        #num = str(num)
+        self.card = suit, num
+
+
+
+class preflop:
+
+    def __init__(self, suit, num):
+        self.suit = ""
+        self.num = 0
+        self.card = suit, num
+
+
+
+def preflop_hand(suit1, num1, suit2, num2):
+    #print(preflop1.card, preflop2.card)
+    if suit1 == suit2:
+        print("You are suited!")
+
+    if num1 == num2:
+        print("You are paired!")
+
+
+    if num1 == num2 + 1 or num1 == num2 - 1:
+       print("You are connected!")
+
+    #print('Your hand is :', suit1, num1, suit2, num2)
+
+
+def check_hand(card1,card2,card3,card4,card5,card6,card7):
+    allcards = []
+    allcards = [card1,card2,card3,card4,card5,card6,card7]
+    #print(allcards)
+    #print(allcards[0][0])
+
+    count = 1
+    number = 1
+    while count <= 5:
+        if allcards[2][0] == allcards[count][0]:
+            number += 1
+        if number == 5:
+            print("You are suited!")
+            break
+        count += 1
+
+
+
+
+
+
+preflop1 = preflop('c', 1)
+preflop2 = preflop('d', 1)
+preflop_hand(preflop1.card[0], preflop1.card[1], preflop2.card[0], preflop2.card[1])
+
+flop1 = community_card('h', 1)
+flop2 = community_card('h', 13)
+flop3 = community_card('h', 12)
+turn = community_card('h', 11)
+river = community_card('h', 10)
+
+check_hand(preflop1.card,preflop2.card,flop1.card,flop2.card,flop3.card,turn.card,river.card)
+#print(flop1.card, river.card)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
