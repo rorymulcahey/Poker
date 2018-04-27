365cdf3feacc09329994003d0e39b31120853678
brickrop2 <34317184+brickrop2@users.noreply.github.com>
2018-04-27 14:02:02 -0800

    finish compare hand strength and tie break functions; still need to announce more than one winner; GUI next

--- PokerHands_V2.py
+++ PokerHands_V2.py
@@ -1,5 +1,5 @@
+"""
 
-'''
 Input Notation
 c : clubs
 d : diamonds
@@ -29,15 +29,15 @@ eg: Two = 1; Ace = 0 or 13
 
 =================================================================================
 To do list:
-    Accept two hands as input and then compare the two
-        - Need to accept multiple hands and store them.
-        - Consider using *args or **kwargs
-        - May want to store number of hands in play, use "If hand is None:" to eliminate empty seat positions
-        - Use the classes to create arrays
+    Announce more than one winner if needed, and match winning hand to seat position.
     Build GUI to facilitate hand comparison
     Include chance to win percentages to show hand strength from current community board
 
 Notes:
+    Accept two hands as input and then compare the two** Done
+    - Need to accept multiple hands and store them.
+    - Consider using *args or **kwargs
+    - May want to store number of hands in play, use "If hand is None:" to eliminate empty seat position
     Show the 5 best cards with the type of hand** Done
     - Reference the original 7 cards for the final hand by reformatting get_all_cards function
     - Work on straight flush func
@@ -48,36 +48,37 @@ Notes:
     Goal: Try to use any number of cards (preflop, flop, turn, river)** Done
 =================================================================================
 
-'''
+"""
 
 
-def get_card(card_number):
-    if card_number == 0 or card_number == 13:
-        return "Ace"
-    elif card_number == 1:
-        return "Two"
-    elif card_number == 2:
-        return "Three"
-    elif card_number == 3:
-        return "Four"
-    elif card_number == 4:
-        return "Five"
-    elif card_number == 5:
-        return "Six"
-    elif card_number == 6:
-        return "Seven"
-    elif card_number == 7:
-        return "Eight"
-    elif card_number == 8:
-        return "Nine"
-    elif card_number == 9:
-        return "Ten"
-    elif card_number == 10:
-        return "Jack"
-    elif card_number == 11:
-        return "Queen"
-    elif card_number == 12:
-        return "King"
+# remove 'None' hands from array
+def all_hands(hands):
+    hand_position = [0] * 10
+    for x in range(0, 10):
+        if hands[x][0][0] is not None or hands[x][1][0] is not None:
+            hand_position[x] = 1
+    index = 0
+    # check array_length for debugs
+    array_length = 11
+    while index < array_length and hands:
+        if hands[index][0][0] is None or hands[index][1][0] is None:
+            del hands[index]
+            array_length -= 1
+            index -= 1
+        index += 1
+    return hands, index, hand_position
+
+
+# goal: output should be an array that include the community cards with the hand
+def create_player_hands(hand_array, community_cards, index):
+    array = []
+    for z in range(0, index):
+        array.append([])
+        for x in range(0, 2):
+            array[z].append(hand_array[z][x])
+        for y in range(0, 5):
+            array[z].append(community_cards[y])
+    return array
 
 
 # Return final 5 cards for the hand. Be sure to send in lists as arguments
@@ -85,14 +86,10 @@ def get_all_cards(all_cards, hand_cards, high_cards):
     final_cards = []
     for z in range(0, len(all_cards)):
         for x in range(0, len(hand_cards)):
-            # print(hand_cards)
-            # need to include Ace into this function (it cant read 13, needs to read 0)
             if hand_cards[x] == 13:
                 hand_cards[x] = 0
-                # print(hand_cards)
             if all_cards[z][1] - 1 == hand_cards[x]:
                 final_cards.append(all_cards[z])
-                # print(len(final_cards))
             if len(final_cards) == 5:
                 print(final_cards)
                 return final_cards
@@ -101,7 +98,6 @@ def get_all_cards(all_cards, hand_cards, high_cards):
                 high_cards[y] = 0
             if all_cards[z][1] - 1 == high_cards[y] and high_cards:
                 final_cards.append(all_cards[z])
-                # print(len(final_cards))
             if len(final_cards) == 5:
                 print(final_cards)
                 return final_cards
@@ -134,13 +130,11 @@ def cards_number_array(all_cards):
     return array
 
 
-# consider how we will compare high cards between hands
 def check_high_card(array, index):
     i = 1
     high_cards = []
     for x in range(13, 0, -1):
         if array[x] == 1:
-            # high_card["rank{0}".format(i)] = x
             high_cards.append(x)
             i += 1
             if i > index:
@@ -203,12 +197,9 @@ def check_straight(array):
             number = 0
             for y in range(0, 5):
                 if 0 <= x-y < 14 and array[x-y] >= 1:
-                    #print(x-y)
                     number += 1
                     straight_cards.append(x-y)
                 else:
-                    #print("break")
-                    #print(number)
                     break
             if number == 5:
                 straight = True
@@ -218,7 +209,6 @@ def check_straight(array):
     return straight, straight_cards
 
 
-# assign values of 1 or more for suit cards in play and zeros for cards not in play, returns true if suit >= 5:
 def check_flush(all_cards):
     array = [0] * 4
     suits = ['c', 'd', 'h', 's']
@@ -275,7 +265,6 @@ def check_full_house(array):
     return have_pair and have_trips, trips_card, pair_card
 
 
-# Check hand for four of a kind
 def check_quads(array):
     quads = False
     quads_card = []
@@ -288,7 +277,6 @@ def check_quads(array):
     return quads, quads_card, tie_breaker_cards
 
 
-# Check hand for straight flush
 def check_straight_flush(array, suit, index):
     suited_array = [0]*14
     for x in range(0, index):
@@ -380,62 +368,54 @@ def check_hand_strength(number_array, cards_array):
     return current_hand_strength, final_cards
 
 
-# remove 'None' hands from array
-def all_hands(hands):
-    index = 0
-    # check array_length for debugs
-    array_length = 11
-    while index < array_length and hands:
-        if hands[index][0][0] is None or hands[index][1][0] is None:
-            del hands[index]
-            array_length -= 1
-            index -= 1
-        index += 1
-    return hands, index
-
-
-# goal: output should be an array that include the community cards with the hand
-def create_player_hands(hand_array, community_cards, index):
-    array = []
-    for z in range(0, index):
-        array.append([])
-        for x in range(0, 2):
-            array[z].append(hand_array[z][x])
-        for y in range(0, 5):
-            array[z].append(community_cards[y])
-    return array
-
-
-def compare_hand_strength(hand_details):
-    best_hand = hand_details[0]
-    tied_hands = [0, 0]
-    for x in range(1, len(hand_details)):
-        if hand_details[x][0] == best_hand[0]:
-            print('tied hand')
-            # create list that include both hands and type of hand index: [0] = strength, [1] and [2] = hands
-            find_tie_break(tied_hands)  # set equal to best hand
-        if hand_details[x][0] > best_hand[0]:
-            best_hand = hand_details[x]
+def compare_hand_strength(all_hand_details, hand_strength):
+    hand_type = max(hand_strength)
+    hand_occurrences = hand_strength.count(hand_type)
+    best_hand = []
+    if hand_occurrences == 1:
+        for x in range(0, len(hand_strength)):
+            if hand_type == all_hand_details[x][0]:
+                best_hand = all_hand_details[x]
+    elif hand_occurrences == 2:
+        tied_hands = [hand_type]
+        for y in range(0, len(hand_strength)):
+            if hand_type == all_hand_details[y][0]:
+                tied_hands.append(all_hand_details[y][1])
+        best_hand = find_tie_break(tied_hands)
+    elif hand_occurrences > 2:
+        pass
+        # this statement is used to break more than 1 tie
+        # print(len(best_hand)) **this can be used to determine if we need to denote more than 1 winning hand
     print(best_hand)
     return best_hand
 
 
 def find_tie_break(tied_hands):
-    winning_hand = [0, 0]
-    print('tied breaker engaged')
-    # develop a method to send hands into the check_hand function that fits
-    # compare the high cards and see which hand wins
-    # return the winning hand
-    if tied_hands[0] == 0:
-        hand1 = cards_array(tied_hands[1])
-        hand2 = cards_array(tied_hands[2])
+    winning_hand = [tied_hands[0]]
+    # need to determine how to announce more than one tie breaking winner
+    if tied_hands[0] != 1 and tied_hands[0] != 2 and tied_hands[0] != 3 and tied_hands[0] != 7:
+        print('tied breaker engaged')
+        hand1 = cards_number_array(tied_hands[1])
+        hand2 = cards_number_array(tied_hands[2])
+        # print(hand1)
+        # print(hand2)
+        for x in range(13, 0, -1):
+            if hand1[x] > hand2[x]:
+                winning_hand.append(tied_hands[1])
+                return winning_hand
+            if hand2[x] > hand1[x]:
+                winning_hand.append(tied_hands[2])
+                return winning_hand
+    # return both hands if tie is not broken
+    # could also consider using tie_broken_boolean
+    winning_hand.append(tied_hands[1])
+    winning_hand.append(tied_hands[2])
     return winning_hand
 
 
 def main():
-
     # Cards in play:
-    hand1 = [('c', 8,), ('d', 3)]
+    hand1 = [('s', 9,), ('s', 3)]
     hand2 = [('s', 2), ('h', 7)]
     hand3 = [('s', 1), ('s', 12)]
     hand4 = [(None, None), (None, None)]
@@ -454,22 +434,27 @@ def main():
     # river = (None, None)
     community_cards = [flop1, flop2, flop3, turn, river]
 
-    all_preflop_hands, number_of_hands = all_hands(hands)
+    all_preflop_hands, number_of_hands, hand_positions = all_hands(hands)  # may not need hand positions #
+    # hands are numerically ordered in accordance with their seat position #
+    # this can be used to match winner with seat position #
     player_hands = create_player_hands(all_preflop_hands, community_cards, number_of_hands)
-    hand_strength = []
+    all_hand_details = []
+    only_strengths = []
     for x in range(0, number_of_hands):
-        hand_strength.append([])
+        all_hand_details.append([])
         cards_array = possible_cards(player_hands[x])
         number_array = cards_number_array(cards_array)
 
-        # hand strength is located with print(hand_strength[x][0])
-        # hand cards are located with print(hand_strength[x][1])
-        idx = check_hand_strength(number_array, cards_array)
-        hand_strength[x].append(idx[0])
-        hand_strength[x].append(idx[1])
+        # hand strength is located with print(all_hand_details[x][0])
+        # hand cards are located with print(all_hand_details[x][1])
+        hand_details = check_hand_strength(number_array, cards_array)
+        only_strengths.append(hand_details[0])
+        print(only_strengths)
+        all_hand_details[x].append(hand_details[0])
+        all_hand_details[x].append(hand_details[1])
 
     # Functions to check hand combinations:
-    compare_hand_strength(hand_strength)
+    compare_hand_strength(all_hand_details, only_strengths)
 
 
 if __name__ == "__main__":
@@ -478,6 +463,35 @@ if __name__ == "__main__":
 
 '''
 old code:
+
+
+def get_card(card_number):
+    if card_number == 0 or card_number == 13:
+        return "Ace"
+    elif card_number == 1:
+        return "Two"
+    elif card_number == 2:
+        return "Three"
+    elif card_number == 3:
+        return "Four"
+    elif card_number == 4:
+        return "Five"
+    elif card_number == 5:
+        return "Six"
+    elif card_number == 6:
+        return "Seven"
+    elif card_number == 7:
+        return "Eight"
+    elif card_number == 8:
+        return "Nine"
+    elif card_number == 9:
+        return "Ten"
+    elif card_number == 10:
+        return "Jack"
+    elif card_number == 11:
+        return "Queen"
+    elif card_number == 12:
+        return "King"
 
 
 # test functions
@@ -583,4 +597,18 @@ print(x)
 preflop_hands = [[[None for k in range(2)] for j in range(2)] for i in range(10)]
 pprint.pprint(preflop_hands)
 
+
+def compare_hand_strength(all_hand_details, hand_strength):
+for x in range(1, len(all_hand_details)):
+    tied_hands = []
+    if all_hand_details[x][0] == best_hand[0]:
+        tied_hands.append(best_hand[0])
+        tied_hands.append(best_hand[1])
+        tied_hands.append(all_hand_details[x][1])
+        best_hand = find_tie_break(tied_hands)
+        # include more than 2 tie breaker hands
+        # tied_break_hands = find_tie_break(tied_hands)
+        # best_hand = tie_break_hands
+    if all_hand_details[x][0] > best_hand[0]:
+        best_hand = all_hand_details[x]
 '''
