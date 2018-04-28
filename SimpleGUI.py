a13abb31e40ca1b02b1e3fde4ea9bd1e75211efe
Rory <Administrator@ABODEARCHITECTU.abodecenter.local>
2018-04-28 05:24:33 -0700

    menu bar

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -1,12 +1,42 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
-app = QtGui.QApplication(sys.argv)
 
-window = QtGui.QWidget()
-window.setGeometry(50, 50, 500, 300)
-window.setWindowTitle("PyQT Tuts!")
+class Window(QtGui.QMainWindow):
 
-window.show()
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle("Poker Hands")
+        self.setGeometry(50, 50, 500, 300)
 
-sys.exit(app.exec())
+        extractAction = QtGui.QAction("&Test code", self)
+        extractAction.setShortcut("Ctrl+Q")
+        extractAction.setStatusTip('Leave The App')
+        extractAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+
+        self.home()
+
+    def home(self):
+        btn = QtGui.QPushButton("Quit", self)
+        btn.clicked.connect(self.close_application)
+        btn.resize(50, 30)
+        btn.move(450, 270)
+        self.show()
+
+    def close_application(self):
+        sys.exit()
+
+        
+def run():
+    app = QtGui.QApplication(sys.argv)
+    gui = Window()
+    sys.exit(app.exec())
+
+
+run()
