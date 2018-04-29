e5d2064c1a36b66b32d0291c4da2c26b1b5511d9
Rory <Administrator@ABODEARCHITECTU.abodecenter.local>
2018-04-28 17:12:49 -0700

    button, download bar, other quit options, style options

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -9,7 +9,7 @@ class Window(QtGui.QMainWindow):
         self.setWindowTitle("Poker Hands")
         self.setGeometry(50, 50, 500, 300)
 
-        extractAction = QtGui.QAction("&Test code", self)
+        extractAction = QtGui.QAction("&Quit menu click", self)
         extractAction.setShortcut("Ctrl+Q")
         extractAction.setStatusTip('Leave The App')
         extractAction.triggered.connect(self.close_application)
@@ -23,16 +23,80 @@ class Window(QtGui.QMainWindow):
         self.home()
 
     def home(self):
-        btn = QtGui.QPushButton("Quit", self)
+        btn = QtGui.QPushButton("Quit button", self)
         btn.clicked.connect(self.close_application)
-        btn.resize(50, 30)
+        btn.resize(btn.minimumSizeHint())
         btn.move(450, 270)
+
+        extractAction = QtGui.QAction(QtGui.QIcon(), 'Quit', self)
+        extractAction.triggered.connect(self.close_application)
+
+        self.toolBar = self.addToolBar("Extraction")
+        self.toolBar.addAction(extractAction)
+
+        checkBox = QtGui.QCheckBox('Enlarge Window', self)
+        checkBox.move(100, 50)
+        checkBox.stateChanged.connect(self.enlarge_window)
+
+        self.progress = QtGui.QProgressBar(self)
+        self.progress.setGeometry(200, 80, 250, 20)
+
+        self.btn = QtGui.QPushButton("Download", self)
+        self.btn.move(200, 120)
+        self.btn.clicked.connect(self.download)
+
+        print(self.style().objectName())
+        self.styleChoice = QtGui.QLabel("Windows Vista", self)
+
+        comboBox = QtGui.QComboBox(self)
+        comboBox.addItem("motif")
+        comboBox.addItem("Windows")
+        comboBox.addItem("cde")
+        comboBox.addItem("Plastique")
+        comboBox.addItem("Cleanlooks")
+        comboBox.addItem("Windowsvista")
+
+        comboBox.move(50, 250)
+        self.styleChoice.move(50, 150)
+        comboBox.activated[str].connect(self.style_choice)
+
+
         self.show()
 
-    def close_application(self):
-        sys.exit()
 
-        
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
+
+
+    def download(self):
+        self.completed = 0
+
+        while self.completed < 100:
+            self.completed += 0.0001
+            self.progress.setValue(self.completed)
+
+
+    def enlarge_window(self, state):
+        if state == QtCore.Qt.Checked:
+            self.setGeometry(50, 50, 1000, 600)
+        else:
+            self.setGeometry(50, 50, 500, 300)
+
+    def close_application(self):
+        choice = QtGui.QMessageBox.question(self, 'Close Program', 'Are you sure?',
+                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        if choice == QtGui.QMessageBox.Yes:
+            print("Exiting")
+            sys.exit()
+        else:
+            pass
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.close_application()
+
+
 def run():
     app = QtGui.QApplication(sys.argv)
     gui = Window()
@@ -40,3 +104,7 @@ def run():
 
 
 run()
+
+
+
+
