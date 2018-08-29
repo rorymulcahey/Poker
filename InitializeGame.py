147f62d9cd79839a4ea09b6895e69d427c9da079
Rory <Administrator@ABODEARCHITECTU.abodecenter.local>
2018-08-29 13:28:02 -0700

    adding game functionality

---
+++ InitializeGame.py
@@ -0,0 +1,34 @@
+"""
+ 
+ This  file will contain the needed code to setup and maintain a poker table.
+ Stored here is the information about each players chip count, the location
+ of the button and big blind. Using that information, the program will begin
+ to assign chip counts and cards to each seat of the table. Then it will 
+ collect antes and blinds from the players, preparing the entire game to 
+ be played. 
+ 
+
+"""
+
+
+class InitializeGame:
+    def __init__(self, total_players, chip_counts):
+        self.players = total_players
+        self.c_counts = chip_counts
+        self.seats = self.occupied_seats(self.players)
+        self.initialize()
+
+    def initialize(self):
+        # give out chips
+        # give out cards
+        # place button
+    
+    def occupied_seats:
+        array = []
+        for y in range(0, self.index):
+        return array[]
+
+
+    def setup_next_hand(self):
+
+
